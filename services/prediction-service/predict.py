d9008e009e92c747b4f2f66609deb25c41d4d3b3
matthiaslim <matthias.limeexuan@gmail.com>
2025-03-19 16:28:42 +0800

    feat: add prediction service Dockerfile

--- services/prediction-service/predict.py
+++ services/prediction-service/predict.py
@@ -110,4 +110,4 @@ def retrain_models():
 threading.Thread(target=retrain_models, daemon=True).start()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3002)
